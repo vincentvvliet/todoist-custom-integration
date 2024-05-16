1babdaf61f60e2643426bde5b91fedfba532766d
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2024-05-16 16:10:58 +0200

    Complete Google Calendar API functionality

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
 import requests
-
+from quickstart import process_events
 from dotenv import load_dotenv
 
 def setup_todoist():
     # Use the API key to authenticate with Todoist API
     todoist_api_key = os.getenv("TODOIST_API_KEY")
-    sync_token = '*'
-    resource_types = '["items"]'
 
     # Prepare the headers with the bearer token
     headers = {
@@ -16,8 +14,8 @@ def setup_todoist():
 
     # Prepare the data for the request
     data = {
-        "sync_token": sync_token,
-        "resource_types": resource_types
+        "sync_token": '*',
+        "resource_types": '["items"]',
     }
 
     return headers, data
@@ -35,26 +33,14 @@ def get_completed_tasks(headers, data):
     else:
         # Todoist API returned an error response
         print(f"Todoist API authentication failed with status code {response.status_code}")
-        print(response.text)  # Print the error response
+        print(response.text)
         return None
 
     # Parse the JSON response
     response_data = response.json()
 
     # Access the relevant data from the response
-    items = response_data.get('items', [])
-
-    return items
-
-def find_event_ids(tasks):
-    base_url = os.getenv('GOOGLE_CALENDAR_BASE_URL')
-    ids = []
-    for task in tasks:
-        print(task)
-        response = requests.post(f"{base_url}calendars/calendarId/events")
-        ids.append()
-
-    return ids
+    return response_data.get('items', [])
 
 def main():
     # Load environment variables from .env file
@@ -69,17 +55,8 @@ def main():
     # Filter for only non google calendar tasks
     completed_todoist_tasks = [task.get('content', '') for task in completed_tasks if not '@GCal' in task.get('content', '')]
 
-    # Find event ID's
-    ids = find_event_ids(completed_todoist_tasks)
-
-    # print("\nCompleted tasks originating from Todoist:")
-    # for task in completed_todoist_tasks:
-    #     print(task)
-    #     response = requests.post(f"{google_calendar_base_url}calendars/calendarId/events", headers=headers, data=data)
-
-
-    # TODO: Retrieve event from Google Calendar corresponding to the task
-    # TODO: Remove event from Google Calendar (Events:delete)
+    # Process events
+    process_events(completed_todoist_tasks)
 
 if __name__ == '__main__':
     main()
--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,3 @@
-import datetime
 import os.path
 
 from google.auth.transport.requests import Request
@@ -11,59 +10,72 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
-def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+def process_events(tasks: list) -> None:
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  try:
-    service = build("calendar", "v3", credentials=creds)
+    try:
+        service = build("calendar", "v3", credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=now,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
+        # Call the Calendar API
+        print("Retrieving ID's...")
 
-    if not events:
-      print("No upcoming events found.")
-      return
+        ids = []
+        for task in tasks:
+            events_result = (
+                service.events()
+                .list(
+                    calendarId="primary",
+                    singleEvents=True,
+                    orderBy="startTime",
+                    q=task,
+                )
+                .execute()
+            )
+            events = events_result.get("items", [])
+            if not events:
+                print("No upcoming events found.")
+                # return
 
-    # Prints the start and name of the next 10 events
-    for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
+            # Prints the start and name of the next 10 events
+            for event in events:
+                print(event["summary"])
 
-  except HttpError as error:
-    print(f"An error occurred: {error}")
+                ids.append(event["id"])
 
+        print("Finalized ID retrieval, the following ID's were found.")
+        print(ids)
 
-if __name__ == "__main__":
-  main()
+        if len(ids) == 0:
+            print("No events found to be deleted")
+            return
+
+        for id in ids:
+            print("Currently deleting event with ID {}".format(id))
+
+            # Delete event
+            service.events().delete(calendarId='primary', eventId=id).execute()
+
+        print("Successfully deleted events.")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
